bfa9aa461326d360eb75d7a5389427ec4ede178e
Toshiaki Maki <makingx@gmail.com>
2015-08-09 03:05:00 +0900

    polish

--- src/main/java/cleaning/type/CleaningTypeRepository.java
+++ src/main/java/cleaning/type/CleaningTypeRepository.java
@@ -1,6 +1,5 @@
 package cleaning.type;
 
-import cleaning.type.CleaningType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CleaningTypeRepository extends JpaRepository<CleaningType, Integer> {
