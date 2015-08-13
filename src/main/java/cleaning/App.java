2b6cef4b2341f15eaa435b5457fb11cb6113f3e5
Toshiaki Maki <makingx@gmail.com>
2015-08-13 21:22:48 +0900

    Forgot to add @ EnableScheduling...

--- src/main/java/cleaning/App.java
+++ src/main/java/cleaning/App.java
@@ -3,9 +3,11 @@ package cleaning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableOAuth2Sso
+@EnableScheduling
 public class App {
 
     public static void main(String[] args) {
